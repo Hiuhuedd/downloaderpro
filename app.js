6cf850d2a2d03dccdec07a2974ee50e73ffcc578
hiuhu <hiuhuedd@gmail.com>
2024-02-18 20:00:30 +0300

    all platforms

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const ytdl = require('ytdl-core');
+
+const bodyParser = require('body-parser');
+const {fetchVideo} = require('@prevter/tiktok-scraper');
 const { initializeApp } = require('firebase/app');
 const { getStorage, ref, uploadBytes, getDownloadURL } = require('firebase/storage');
+const { ndown,tikdown,ytdown } = require("nayan-media-downloader")
 
 const app = express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 const port = 3000;
 
 // Increase the timeout to 10 minutes (adjust as needed)
@@ -23,69 +31,116 @@ const firebaseConfig = {
 const firebaseApp = initializeApp(firebaseConfig);
 const storage = getStorage(firebaseApp);
 
-app.get('/download', async (req, res) => {
-  const startTime = Date.now();
+app.post('/download', async (req, res) => {
+      const link = req.body.url;
 
   try {
-    const videoUrl = req.query.url;
+    // Check the platform and call the appropriate method based on the platform
+    if (link.includes('youtu.be')) {
+      try {
+        
+        let URL2 = await ytdown(link)
+        console.log(URL2);
+         const resObject={
+           thumbnail: URL2.data.picture,
+           title: URL2.data.title,
+           description: URL2.data.published,
+           url:URL2.data.video,
+           source:"Youtube",
+           color:"#dc2626"
+         }
+           res.status(200).json(resObject);
+       
+      } catch (error) {
+        console.log(error);
+        
+      }
+        
+      } else if (link.includes('tiktok')) {
+   try {
+          
+          try {
+        
+            const response= await fetchVideo(link);
+         
+            console.log(response);
+             const resObject={
+               thumbnail: response.previewImageUrl,
+               title: "TikTok",
+               description:response.description,
+               url:response.videoWatermark.url,
+               source:"TikTok",
+               color:"#111"
 
-    if (!ytdl.validateURL(videoUrl)) {
-      throw new Error('Invalid YouTube URL');
+             }
+               res.status(200).json(resObject);
+           
+          } catch (error) {
+            console.log(error);
+            
+          }
+       
+      } catch (error) {
+          console.log(error);
+      }
+        
+      } else if (link.includes('instagram')) {
+        
+        try {
+        
+          let URL = await ndown(link)
+          const response=URL.data[1]||URL.data[0]
+          console.log(response, URL.data);
+           const resObject={
+             thumbnail: response.thumbnail,
+             title: "Instagram",
+             description:"Instagram download",
+             url:response.url,
+             source:"Instagram",
+             color:"#ec4899"
+             
+            }
+             res.status(200).json(resObject);
+             
+            } catch (error) {
+              console.log(error);
+              
+            }
+            
+            
+          } else if ((link.includes("fb"))|| (link.includes("facebook"))) {
+            
+       
+          try {
+        
+            let URL = await ndown(link)
+            const response=URL.data[0]
+            console.log(response);
+             const resObject={
+               thumbnail: response.thumbnail,
+               title: "FaceBook",
+               description:"Facebook download",
+               url:response.url,
+               source:"FaceBook",
+               color:"#2563eb"
+          
+             }
+               res.status(200).json(resObject);
+           
+          } catch (error) {
+            console.log(error);
+            
+          }
+        
+
+    }else{
+      res.status(400).send("Platform not found");
+
     }
-
-    // Get video information
-    const info = await ytdl.getInfo(videoUrl);
-    
-    // Access video details
-    const title = info.videoDetails.title;
-    const description = info.videoDetails.description;
-    const thumbnails = info.videoDetails.thumbnails;
-
-    console.log('Video :', info.videoDetails);
-    // console.log('Video Title:', title);
-    // console.log('Video Description:', description);
-    // console.log('Thumbnails:', thumbnails);
-
-    // Choose the format for downloading
-    const format = ytdl.chooseFormat(info.formats, { quality: 'highest', filter: 'audioandvideo' });
-
-    // Download the video stream
-    const videoStream = ytdl(videoUrl, { format });
-
-    // Convert stream to buffer
-    const chunks = [];
-    videoStream.on('data', (chunk) => {
-      console.log("piping");
-      chunks.push(chunk);
-    });
-
-    videoStream.on('end', async () => {
-      const videoBuffer = Buffer.concat(chunks);
-console.log("uploading to firebase");
-      // Upload the video to Firebase Storage
-      const fileRef = ref(storage, `videos/${title}.mp4`);
-      await uploadBytes(fileRef, videoBuffer);
-
-      // Get the download URL
-      const downloadURL = await getDownloadURL(fileRef);
-
-      console.log('Firebase Storage download URL:', downloadURL);
-      const endTime = Date.now();
-      const downloadTime = endTime - startTime;
-
-      console.log(`Download time: ${downloadTime} ms`);
-
-      res.status(200).json({
-        title,
-        description,
-        thumbnails,
-        filepath:downloadURL
-      });
-    });
-  } catch (error) {
-    console.error(error.message);
-    res.status(400).send(error.message);
-  }
+    }catch (error) {
+          console.error(error.message);
+          res.status(400).send(error.message);
+        }
 });
 
 app.listen(port, () => {
@@ -93,86 +148,3 @@ app.listen(port, () => {
 });
 
 
-// const express = require('express');
-// const ytdl = require('ytdl-core');
-// const fs = require('fs');
-// const path = require('path');
-
-// const app = express();
-// const port = 3000;
-
-// // Increase the timeout to 10 minutes (adjust as needed)
-// app.timeout = 600000; // 10 minutes in milliseconds
-
-// // Assuming you have a static directory for serving videos
-// app.use('/videos', express.static(path.join(__dirname, 'videos')));
-
-// app.get('/download', async (req, res) => {
-//   const startTime = Date.now();
-
-//   try {
-//     const videoUrl = req.query.url;
-
-//     if (!ytdl.validateURL(videoUrl)) {
-//       throw new Error('Invalid YouTube URL');
-//     }
-
-//     // Get video information
-//     const info = await ytdl.getInfo(videoUrl);
-
-//     // Access video details
-//     const title = info.videoDetails.title;
-//     const description = info.videoDetails.description;
-//     const thumbnails = info.videoDetails.thumbnails;
-
-//     console.log('Video:', info.videoDetails);
-
-//     // Choose the format for downloading
-//     const format = ytdl.chooseFormat(info.formats, { quality: 'highest', filter: 'audioandvideo' });
-
-//     // Create the videos directory if it doesn't exist
-//     const videosDirectory = path.join(__dirname, 'videos');
-//     if (!fs.existsSync(videosDirectory)) {
-//       fs.mkdirSync(videosDirectory);
-//     }
-
-//     // Download the video stream
-//     const videoStream = ytdl(videoUrl, { format });
-//     if (videoStream) {
-//       console.log("Downloaded the video stream");
-//     }
-//     // Create a write stream to save the file to the server
-//     const filePath = path.join(videosDirectory, `${title}.mp4`);
-//     const writeStream = fs.createWriteStream(filePath);
-  
-//     videoStream.pipe(writeStream);
-//     console.log("piping the video stream");
-    
-//     // Wait for the write stream to finish
-//     writeStream.on('finish', () => {
-//       console.log('Download completed');
-
-//       const endTime = Date.now();
-//       const downloadTime = endTime - startTime;
-
-//       console.log(`Download time: ${downloadTime} ms`);
-
-//       // Set response headers
-//       const publicUrl = `${req.protocol}://${req.get('host')}/videos/${title}.mp4`;
-//       res.status(200).json({
-//         title,
-//         description,
-//         thumbnails,
-//         filePath: publicUrl // Send the public URL for the client to access the file
-//       });
-//     });
-
-//   } catch (error) {
-//     console.error(error.message);
-//     res.status(400).send(error.message);
-//   }
-// });
-
-// app.listen(port, () => {
-//   console.log(`Server is running at http://localhost:${port}`);
-// });
